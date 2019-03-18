23efece8d58634cca0eda70a28663f67840f7b97
datfc97pro <datfc97pro@gmail.com>
2019-03-18 17:13:40 +0700

    back-supreme

---
+++ src/resolvers/Query.js
@@ -0,0 +1,64 @@
+const { forwardTo } = require("prisma-binding");
+const { hasPermission } = require("../utils");
+
+const Query = {
+  items: forwardTo("db"),
+  item: forwardTo("db"),
+  itemsConnection: forwardTo("db"),
+  me: async (parent, args, ctx, info) => {
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return await ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
+  },
+  users: async (parent, args, ctx, info) => {
+    if (!ctx.request.userId) {
+      throw new Error("You");
+    }
+
+    hasPermission(ctx.request.user, ["ADMIN", "PERMISSIONUPDATE"]);
+
+    return ctx.db.query.users({}, info);
+  },
+  order: async (parent, args, ctx, info) => {
+    if (!ctx.request.userId) {
+    }
+
+    const order = await ctx.db.query.order(
+      {
+        where: { id: args.id }
+      },
+      info
+    );
+
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes(
+      "ADMIN"
+    );
+
+    if (!ownsOrder || !hasPermissionToSeeOrder) {
+      throw new Error(`You cant see this buddd`);
+    }
+
+    return order;
+  },
+  orders: async (parent, args, ctx, info) => {
+    const { userId } = ctx.request;
+
+    if (!userId) throw new Error(`You arent logged in!`);
+
+    return await ctx.db.query.orders(
+      {
+        where: { user: { id: userId } }
+      },
+      info
+    );
+  }
+};
+
+module.exports = Query;
